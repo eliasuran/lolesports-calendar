a30207e419eb8808d404cc507edae69da7ba8d91
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-15 11:27:18 +0200

    added test event to calendar

--- api/main.go
+++ api/main.go
@@ -27,9 +27,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Calendars:")
-	for _, item := range calendarList.Items {
-		fmt.Println(item.Id)
+	event := &calendar.Event{
+		Summary:     "Google I/O 2015",
+		Location:    "800 Howard St., San Francisco, CA 94103",
+		Description: "A chance to hear more about Google's developer products.",
+		Start: &calendar.EventDateTime{
+			DateTime: "2015-05-28T09:00:00-07:00",
+			TimeZone: "America/Los_Angeles",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: "2015-05-28T17:00:00-07:00",
+			TimeZone: "America/Los_Angeles",
+		},
+		Recurrence: []string{"RRULE:FREQ=DAILY;COUNT=2"},
+		Attendees: []*calendar.EventAttendee{
+			&calendar.EventAttendee{Email: "lpage@example.com"},
+			&calendar.EventAttendee{Email: "sbrin@example.com"},
+		},
 	}
 
+	calendarId := calendarList.Items[0].Id
+	event, err = srv.Events.Insert(calendarId, event).Do()
+	if err != nil {
+		fmt.Printf("Couldnt create event: %v\n", err)
+	}
+
+	fmt.Printf("Event created: %s\n", event.HtmlLink)
 }
