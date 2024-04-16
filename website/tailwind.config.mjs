20ee38c0458f44057cff1664c3c2d35593267c3a
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-16 19:29:38 +0200

    started on  frontend

---
+++ website/tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    colors: {
+      bg: "#0F151A",
+      text: "#FFFFFF",
+      primary: "#0A0E14",
+      secondary: "#111111"
+    },
+	},
+	plugins: [],
+}
