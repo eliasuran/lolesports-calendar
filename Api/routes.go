a1dc91ee2bb2ffc47e1692f0d5a588ae2951d996
Elias Rissanen Uran <elura001@osloskolen.no>
2024-04-18 13:15:20 +0200

    moved calendar api to api

--- Api/routes.go
+++ Api/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	calendarapi "github.com/eliasuran/lolesports-calendar-api/CalendarApi"
 )
 
 func addRoutes(
@@ -9,6 +11,9 @@ func addRoutes(
 	dataPath string,
 ) {
 	mux.Handle("GET /", http.NotFoundHandler())
+	mux.HandleFunc("POST /auth", func(w http.ResponseWriter, r *http.Request) {
+		calendarapi.Authorize()
+	})
 	mux.HandleFunc("GET /leagues", func(w http.ResponseWriter, r *http.Request) {
 		get_active_leagues(w, r, dataPath)
 	})
